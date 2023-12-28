77ca18447d11c4b3a9e841804d02ee83f2ad75ce
Harry Day <contact@harryday.xyz>
2023-12-28 15:32:16 +0000

    Added some indent options

--- init.lua
+++ init.lua
@@ -323,6 +323,11 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 -- [[ My settings ]]
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.incsearch = true
@@ -332,6 +337,7 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.opt.scrolloff = 8
 
 -- [[ Basic Keymaps ]]
 
